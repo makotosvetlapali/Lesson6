cb19d4fda0c2a3c94e014c25d2e51ead8132e866
makotosvetlapali <donpedjas@gmail.com>
2019-06-16 18:04:28 +0200

    Second change

--- src/exercises/Algorithms.java
+++ src/exercises/Algorithms.java
@@ -14,4 +14,39 @@ public class Algorithms {
 		return -1;
 	}
 
+	public static Object countPearls(List<Boolean> oysters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object containsSOS(List<String> message1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<String> sortScores(List<Double> results) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object sortDNA(List<String> unsortedSequences) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }

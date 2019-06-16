affabaf1fee6607a108b2b998fb98fa4e42ee808
makotosvetlapali <donpedjas@gmail.com>
2019-06-16 18:06:09 +0200

    Second change

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 import junit.framework.TestCase;
 
 public class StringReversal extends TestCase {
@@ -11,8 +13,19 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+		JOptionPane.showMessageDialog(null, reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+
+	}
+
+	private String reverseMe(String string) {
+		String reverse = "";
+		for (int i = string.length() - 1; i >= 0; i--) {
+			reverse = reverse + string.charAt(i);
+		}
+		return reverse;
+
+	}
 
 }
